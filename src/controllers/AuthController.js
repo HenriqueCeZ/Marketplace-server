99e202e1f545f108605a81654fc9ad4902d3cf02
HenriqueCeZ <henriquecezar.jp@gmail.com>
2021-11-05 05:42:29 -0300

    feat: create validators

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -1,9 +1,16 @@
+const {validationResult, matchedData} = require('express-validator')
+
 module.exports = {
         signin: async (req, res) =>{
 
         },
         signup: async(req, res) =>{
-            
+            const errors = validationResult(req)
+            if(!errors.isEmpty()){//se errors  não está vazio 
+                res.json({error: errors.mapped()})
+                return
+            }
+              res.json({tudocerto:true})  
         }
      
 }
