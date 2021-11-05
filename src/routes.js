d27ab4ae746065301d66d173d43dca30c66c3e5c
HenriqueCeZ <henriquecezar.jp@gmail.com>
2021-11-05 04:49:49 -0300

    feat: create user middleware and add to routes

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const AuthController = require('./controllers/AuthController')
 const UserController = require('./controllers/UserController')
 const AdController = require('./controllers/AdController')
+const Auth = require('./middlewares/Auth')
 
 
 
@@ -14,14 +15,14 @@ router.get('/states',UserController.getStates) // endpoint de listar estados
 
 router.post('/user/signin', AuthController.signin)//endpoint de login
 router.post('/user/signup', AuthController.signup)//endpoint de cadastro
-router.get('/user/me', UserController.info)//endpoint de lista infor de usuário
-router.put('/user/me/:id',UserController.editAction)//endpoint de editar infor do usuário
+router.get('/user/me',Auth.private, UserController.info)//endpoint de lista infor de usuário
+router.put('/user/me/:id',Auth.private, UserController.editAction)//endpoint de editar infor do usuário
 
 
 router.get('/categories', AdController.getCategories)//endpoint de listar categorias
-router.post('/ad/add', AdController.addAction)//endpoint de add anuncio
+router.post('/ad/add',Auth.private, AdController.addAction)//endpoint de add anuncio
 router.get('/ad/list', AdController.getList)//endpoint de listar anuncio
 router.get('/ad/item', AdController.getItem)//endpoint de listar informação de um  item específico
-router.post('/ad/:id', AdController.editAction)// endpoint de editar infor de anúncio
+router.post('/ad/:id',Auth.private, AdController.editAction)// endpoint de editar infor de anúncio
 
 module.exports = router
